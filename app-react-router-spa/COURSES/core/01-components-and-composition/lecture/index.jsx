959b9953e68ddc90c31a981b55147a8c56e64aef
Robert Pate <robert.pate@ezcater.com>
2025-03-24 12:01:54 -0400

    docs: lesson 1 complete

--- app-react-router-spa/COURSES/core/01-components-and-composition/lecture/index.jsx
+++ app-react-router-spa/COURSES/core/01-components-and-composition/lecture/index.jsx
@@ -5,8 +5,13 @@ import { LessonBody, LessonCard } from '~/Lesson'
 import { Icon } from './examples/Icon'
 import { FaTrash } from 'react-icons/fa'
 
-function App() {
-  // Let's get started
+function Heading({children, text, tag: Comp = "h1", icon: Icon}) {
+  return <Comp className="heading-size-1">{text} <Icon /> {children}</Comp>
 }
 
-const el = document.getElementById('root')
+function App({name, message}) {
+  return <div id="id" className="heading-size-1"><Heading text={name} icon={FaTrash}><FaTrash />this is children<FaTrash /></Heading> <span>{message}</span><FaTrash /></div>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App name="World" message="Hello"/>) // jsx(App) -> react will call this fn

441c37cf2ab8063ff93bd8ff0562723f54db8a6e
Robert Pate <robert.pate@ezcater.com>
2025-03-24 16:54:21 -0400

    exercise: form submission in 2 ways

--- app-react-router-spa/COURSES/core/04-forms/lecture/GroceryForm.tsx
+++ app-react-router-spa/COURSES/core/04-forms/lecture/GroceryForm.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useId, useState, useRef } from 'react'
 
 type Item = {
   name: string
@@ -12,25 +12,42 @@ type Props = {
 export function GroceryForm({ onSubmit }: Props) {
   // Teach refs with typescript
   // Teach React 19 actions
+  const nameId = useId() // :r0:
+  const quantityId = useId() // :r0:
 
-  function handleSubmit(event /* <---- TS has no idea what this is */) {
+  const nameRef = useRef<HTMLInputElement>(null!) // <- required in React 19
+  // ref valeus may not be accessed during rendering
+  // the non-null assertion operator (!) is used to tell TypeScript to type narrow th ref at runtime
+
+  const quantityRef = useRef<HTMLInputElement>(null!)
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
     // Three basic ways to get our form's fields
     // 1. Scrape for it: ids (bad) refs (good)
     // 2. Controlled with state
     // 3. new FormData
-    onSubmit({ name: 'test', quantity: 1 })
+
+    //console.log(nameRef)
+    //onSubmit({ name: nameRef.current.value, quantity: parseInt(quantityRef.current.value) })
+
+
+    const data = new FormData(event.currentTarget)
+    const name = data.get('name')
+    const quantity = data.get('quantity')
+    console.log(name, quantity)
+    onSubmit({ name: name as string, quantity: parseInt(quantity as string) })
   }
 
   return (
     <form onSubmit={handleSubmit} className="space-y-3">
       <div>
-        <label htmlFor="itemName">Item</label>
-        <input id="itemName" type="text" className="form-field" autoComplete="off" name="name" />
+        <label htmlFor={nameId}>Item</label>
+        <input ref={nameRef} id={nameId} type="text" className="form-field" autoComplete="off" name="name" />
       </div>
       <div>
-        <label htmlFor="itemQuantity">Quantity</label>
-        <input id="itemQuantity" type="text" className="form-field" name="quantity" />
+        <label htmlFor={quantityId}>Quantity</label>
+        <input ref={quantityRef} id={quantityId} type="text" className="form-field" name="quantity" />
       </div>
       <footer>
         <button type="submit" className="button">
